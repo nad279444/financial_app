454eb64c48d0e4e1fc8640283d0d9136dc58e33e
nad279444 <nardfrimpong@gmail.com>
2023-04-11 11:55:00 +0000

    set up materia ui theme and configuration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,17 @@
+import { useMemo } from "react"
+import { createTheme } from "@mui/material/styles"
+import { ThemeProvider, CssBaseline } from "@mui/material"
+import { themeSettings } from "./theme"
 
 
 function App() {
+  const theme = useMemo(() => createTheme(themeSettings),[])
   return (
     <div className="app">
+      <ThemeProvider theme = {theme }>
+        <CssBaseline />
+        hi 
+      </ThemeProvider>
      
     </div>
   )
