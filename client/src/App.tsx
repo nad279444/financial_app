4bb096f3be443f93898b7f9934a05c1d84069d16
nad279444 <nardfrimpong@gmail.com>
2023-04-11 13:33:18 +0000

    created a navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,30 @@
 import { useMemo } from "react"
 import { createTheme } from "@mui/material/styles"
 import { ThemeProvider, CssBaseline } from "@mui/material"
+import { Box } from "@mui/material"
 import { themeSettings } from "./theme"
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import Navbar from "@/scenes/navbar";
+import Dashboard from "@/scenes/dashboard";
+import Predictions from "@/scenes/predictions";
 
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings),[])
   return (
     <div className="app">
+    <BrowserRouter>
       <ThemeProvider theme = {theme }>
         <CssBaseline />
-        hi 
+        <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/predictions" element={<Predictions />} />
+            </Routes>
+          </Box>
       </ThemeProvider>
-     
+    </BrowserRouter> 
     </div>
   )
 }
--- client/src/theme-types.ts
+++ client/src/theme-types.ts
@@ -1,4 +1,4 @@
-// eslint-disble-next-line
+// eslint-disable-next-line
 import { Palette,PaletteColor } from "@mui/material/styles/createPalette";
 
 declare module "@mui/material/styles/createPalette"{
