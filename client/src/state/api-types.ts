742639fe7040b1b902be2c396778e2cb1f17856a
nad279444 <nardfrimpong@gmail.com>
2023-04-12 11:09:17 +0000

    created an api for the products data together with its types

--- client/src/state/api-types.ts
+++ client/src/state/api-types.ts
@@ -33,3 +33,14 @@ export interface Month {
     createdAt: string;
     updatedAt: string;
   }
+
+  export interface GetProductsResponse {
+    id: string;
+    _id: string;
+    __v: number;
+    price: number;
+    expense: number;
+    transactions: Array<string>;
+    createdAt: string;
+    updatedAt: string;
+  }
--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,17 +1,22 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { GetKpisResponse } from "./api-types";
+import { GetKpisResponse , GetProductsResponse} from "./api-types";
 
 export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL}),
     reducerPath: "main",
-    tagTypes: ["Kpis"],
+    tagTypes: ["Kpis","Products"],
     endpoints: (build) => ({
       getKpis: build.query<Array<GetKpisResponse>, void>({
         query: () => "kpi/kpis/",
         providesTags: ["Kpis"],
       }),
+      getProducts: build.query<Array<GetProductsResponse>, void>({
+        query: () => "product/products/",
+        providesTags: ["Products"],
+      }),
     }),
+     
   });
 
- export const { useGetKpisQuery } = api
+ export const { useGetKpisQuery, useGetProductsQuery } = api
   
